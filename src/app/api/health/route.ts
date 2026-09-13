b144815e0e24effefe149a14d5285e4624a3730c
OmarMadbouly0 <omarsayed.cpp@gmail.com>
2026-09-13 15:36:31 +0300

    implement Health route and db connection

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,27 @@
+import { checkDatabaseConnection } from "@/lib/db";
+
+export async function GET() {
+  const isConnected = await checkDatabaseConnection();
+
+  if (!isConnected) {
+    return Response.json(
+      {
+        status: "error",
+        message: "Database connection failed",
+      },
+      {
+        status: 503,
+      },
+    );
+  }
+
+  return Response.json(
+    {
+      status: "ok",
+      message: "Database connected successfully",
+    },
+    {
+      status: 200,
+    },
+  );
+}
